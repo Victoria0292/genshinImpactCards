46ded097d45336fea699761403ee45f2c36082ba
Victoria0292 <tlynn0292@gmail.com>
2022-11-11 15:39:03 -0500

    added removeActiveClasses functions

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
-const card = document.querySelectorAll(".card");
+const cards = document.querySelectorAll(".card");
 
-card.forEach(card => {
-    card.addEventListener( 'click', function() {
-      card.classList.toggle('flipCard');
-    });
+//Loops through each card
+cards.forEach((card) => {
+  card.addEventListener("click", function () {
+    card.classList.toggle("cardFlip");
   });
+  //Listening for a click, when click happens function will run
+  card.addEventListener("click", () => {
+    removeActiveClasses();
+    card.classList.add("active");
+  });
+});
+
+//Removes the classes that are not active
+function removeActiveClasses() {
+  //Loops through each card
+  cards.forEach((card) => {
+    card.classList.remove("active");
+  });
+}
